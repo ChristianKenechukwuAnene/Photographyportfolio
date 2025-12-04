3ef7678eb9d6b714e0c8b6fb8b80cac1ab68bba5
ChristianKenechukwuAnene <keecee_a@yahoo.com>
2025-12-04 18:31:51 +0000

    Fix Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Firstportfolio-clean/', // must match your repo name //
+  base: '/Photographyportfolio/', // must match your repo name //
   plugins: [react()],
 })
